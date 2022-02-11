68f79b932af86d62e5cece2e894d5b0567d3db5c
wcappel <wcappel@oxy.edu>
2022-02-10 19:57:10 -0800

    2

--- hw1/HW1.py
+++ hw1/HW1.py
@@ -17,18 +17,102 @@ import matplotlib.pyplot as plt
  do you observe about the eigenvalues of the two matrices?  Compute the determinant
  of the smaller matrix and compare this to the product of all the eigenvalues.
  Write a comment explaining your observations.
+"""
 
+# 1a.
+A = np.random.rand(5, 4)
+AT = np.transpose(A)
+ATA = np.matmul(AT, A)
+AAT = np.matmul(A, AT)
+evATA = LA.eig(ATA)[0].real
+print("ev of A^TA: ")
+print(evATA)
+evAAT = LA.eig(AAT)[0].real
+print("ev of AA^T: ")
+print(evAAT)
+print("Dimensions of ATA: ")
+print(ATA.shape)
+print("Dimensions of AAT: ")
+print(AAT.shape)
+print("ATA is smaller.")
+evProd = np.prod(evATA)
+print("product of eigenvalues: ")
+print(evProd)
+ATAdet = LA.det(ATA)
+print("det. of ATA: ")
+print(ATAdet)
+
+'''AA^T has one more eigenvalue than A^TA, but the other eigenvalues of each
+are equal if you discount the complex numbers in A^TA.
+The determinant of A^TA is equal to the product of all its eigenvalues.'''
+
+"""
  1b. -- For the smaller matrix above, find the eigenvalues of the inverse and
  compare these to the eigenvalues of the original matrix.  What is their
  relationship?  Demonstrate their relationship by writing some code below.  
  Write a comment explaining the relationship and how your code deomstrates this.
+"""
 
+#1b
+ATAinv = LA.inv(ATA)
+# print(ATAinv)
+evATAinv = LA.eig(ATAinv)[0].real
+print("original ev: ")
+print(evATA)
+print("inverse ev: ")
+print(evATAinv)
+print("inverse of inverse ev:")
+check = []
+for ev in evATAinv:
+    check.append(float(1/ev.item()))
+print(check)
+
+'''My code takes the eigenvalues of the inverted matrix and inverts them.
+The result is equal to the eigenvalues of the original matrix.
+This displays that the eigenvalues of an inverse matrix are the inverse
+of the original matrix's eigenvalues'''
+
+"""
  1c. -- Initialize a random, square, non-symmetric matrix C.  Find the 
  eigenvalues of both C and its transpose.  What do you notice about the 
  eigenvalues for both matrices?  Show that they are the same by showing that
  the sum of their square differences amounts to a floating point error.  
  NOTE: you will likely need to sort both arrays.
+"""
 
+#1c
+C = 0
+while 1:
+    temp = np.random.rand(3, 3)
+    if not np.array_equal(temp, np.transpose(temp)):
+        C = temp
+        break
+
+CT = np.transpose(C)
+evC = LA.eig(C)[0].real
+print("eigenvalues of C:")
+print(evC)
+evCT = LA.eig(CT)[0].real
+print("eigenvalues of CT:")
+print(evCT)
+np.sort(evC)
+np.sort(evCT)
+
+def sumofsd(v, u):
+    sum = 0
+    for i, value in enumerate(v):
+        sum += (v[i] - u[i]) ** 2
+    return sum
+
+print(sumofsd(evC, evCT))
+'''
+The eigenvalues for C and its transpose are equal. The sum of square 
+differences must be 0, since they are the same values, yet it is an
+astronomically low number, approximate to 0. This clearly displays
+that this was a floating point error due to the computer.
+'''
+
+"""
  1d. -- Finding the eigenvalues and eigenvectors of a matrix is one example of
  a MATRIX DECOMPOSITION.  There are in fact many ways to decompose a matrix
  into a product of other matrices (the factors usually possessing some
@@ -37,11 +121,12 @@ import matplotlib.pyplot as plt
  of the factors by writing code that demonstrates their structure.  Use the 
  internet to find formal and informal articles explaining the various 
  decompositions and cite your sources.  Write your ideas in a comment for 
- each one below:"""
+ each one below:
+ """
 
 #1d
 # Initialize matrix D:  
-D = np.random.randint(-5, 5, size=(3, 3))
+D = np.random.rand(3, 3)
 
 # LU Factorization
 print("### LU ###")
@@ -110,82 +195,6 @@ Reference: https://www.cs.princeton.edu/courses/archive/spring12/cos598C/svdchap
 '''
 
 
-# 1a.
-A = np.random.randint(-5, 5, size=(5, 4))
-AT = np.transpose(A)
-ATA = np.matmul(AT, A)
-AAT = np.matmul(A, AT)
-evATA = LA.eig(ATA)[0].real
-print("ev of A^TA: ")
-print(evATA)
-evAAT = LA.eig(AAT)[0].real
-print("ev of AA^T: ")
-print(evAAT)
-print("Dimensions of ATA: ")
-print(ATA.shape)
-print("Dimensions of AAT: ")
-print(AAT.shape)
-print("ATA is smaller.")
-evProd = np.prod(evATA)
-print("product of eigenvalues: ")
-print(evProd)
-ATAdet = LA.det(ATA)
-print("det. of ATA: ")
-print(ATAdet)
-'''AA^T has one more eigenvalue than A^TA, but the other eigenvalues of each
-are equal if you discount the complex numbers in A^TA.
-The determinant of A^TA is equal to the product of all its eigenvalues.'''
-
-#1b
-ATAinv = LA.inv(ATA)
-# print(ATAinv)
-evATAinv = LA.eig(ATAinv)[0].real
-print("original ev: ")
-print(evATA)
-print("inverse ev: ")
-print(evATAinv)
-print("inverse of inverse ev:")
-check = []
-for ev in evATAinv:
-    check.append(float(1/ev.item()))
-print(check)
-'''My code takes the eigenvalues of the inverted matrix and inverts them.
-The result is equal to the eigenvalues of the original matrix.
-This displays that the eigenvalues of an inverse matrix are the inverse
-of the original matrix's eigenvalues'''
-
-#1c
-C = 0
-while 1:
-    temp = np.random.randint(-5, 5, size=(3, 3))
-    if not np.array_equal(temp, np.transpose(temp)):
-        C = temp
-        break
-
-CT = np.transpose(C)
-evC = LA.eig(C)[0].real
-print("eigenvalues of C:")
-print(evC)
-evCT = LA.eig(CT)[0].real
-print("eigenvalues of CT:")
-print(evCT)
-np.sort(evC)
-np.sort(evCT)
-
-def sumofsd(v, u):
-    sum = 0
-    for i, value in enumerate(v):
-        sum += (v[i] - u[i]) ** 2
-    return sum
-
-print(sumofsd(evC, evCT))
-'''
-The eigenvalues for C and its transpose are equal. The sum of square 
-differences must be 0, since they are the same values, yet it is an
-astronomically low number, approximate to 0. This clearly displays
-that this was a floating point error due to the computer.
-'''
-
 # Problem 2 - Run Times and Efficiency
 #------------------------------------------------------------------------------
 """It turns out that inverting a matrix using LA.solve 
@@ -195,7 +204,7 @@ that this was a floating point error due to the computer.
  samples for each size.  Record the amount of time each trial takes in two 
  separate arrays and plot their histograms (with annotations and title).  You 
  can randomly initialize a matrix using any method you wish, but using np.random.rand
- is especially convenient.    
+ is especially convenient.
 
  Note: In plotting your histograms use the 'alpha' parameter to adjust the 
  color transparency so one can easily see the overlapping parts of your graphs.
@@ -203,4 +212,94 @@ that this was a floating point error due to the computer.
  Some references: 
  https://scicomp.stackexchange.com/questions/22105/complexity-of-matrix-inversion-in-numpy
  https://docs.scipy.org/doc/scipy/reference/tutorial/linalg.html"""
+
+# So make 5 diff. histograms based on each matrix size
+# Every histogram will be made from the 2 arrays containing inv and solve runtimes
+# Set bins to auto in .hist()
+
+def runtimes(samplenum, shape):
+    invResults = []
+    solResults = []
+    for i in range(samplenum):
+        matrix = np.random.rand(shape, shape)
+        b = np.random.rand(shape)
+        invStart = time.time()
+        LA.inv(matrix)
+        invEnd = time.time()
+        solStart = time.time()
+        LA.solve(matrix, b)
+        solEnd = time.time()
+        invResults.append(invEnd - invStart)
+        solResults.append(solEnd - solStart)
+    return [invResults, solResults]
+
+print("running samples...")
+shape5 = runtimes(1000, 5)
+inv5 = np.array(shape5[0])
+sol5 = np.array(shape5[1])
+
+shape10 = runtimes(1000, 10)
+inv10 = np.array(shape10[0])
+sol10 = np.array(shape10[1])
+
+shape20 = runtimes(1000, 20)
+inv20 = np.array(shape20[0])
+sol20 = np.array(shape20[1])
+
+shape50 = runtimes(1000, 50)
+inv50 = np.array(shape50[0])
+sol50 = np.array(shape50[1])
+
+shape200 = runtimes(1000, 200)
+inv200 = np.array(shape200[0])
+sol200 = np.array(shape200[1])
+
+# Runtimes for 5x5 matrix
+plt.hist(inv5, label=".inv", alpha=0.7)
+plt.hist(sol5, label=".solve", alpha=0.7)
+plt.title("Runtimes for 5x5 Matrix")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Samples")
+plt.legend()
+plt.show()
+
+# Runtimes for 10x10 matrix
+plt.hist(inv10, label=".inv", alpha=0.7)
+plt.hist(sol10, label=".solve", alpha=0.7)
+plt.title("Runtimes for 10x10 Matrix")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Samples")
+plt.legend()
+plt.show()
+
+# Runtimes for 20x20 matrix
+plt.hist(inv20, label=".inv", alpha=0.7)
+plt.hist(sol20, label=".solve", alpha=0.7)
+plt.title("Runtimes for 20x20 Matrix")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Samples")
+plt.legend()
+plt.show()
+
+# Runtimes for 50x50 matrix
+plt.hist(inv50, label=".inv", alpha=0.7)
+plt.hist(sol50, label=".solve", alpha=0.7)
+plt.title("Runtimes for 50x50 Matrix")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Samples")
+plt.legend()
+plt.show()
+
+# Runtimes for 200x200 matrix
+plt.hist(inv200, label=".inv", alpha=0.7)
+plt.hist(sol200, label=".solve", alpha=0.7)
+plt.title("Runtimes for 200x200 Matrix")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Samples")
+plt.legend()
+plt.show()
+
+
+
+
 
